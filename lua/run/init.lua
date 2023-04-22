80b7662df2424404c581a31e9589d16d3f6403b6
zakissimo <zakaria.habri@gmail.com>
2023-04-22 12:23:55 +0200

    Previous input is remembered

--- lua/run/init.lua
+++ lua/run/init.lua
@@ -4,13 +4,18 @@ M.setup = {
     input_map = "<Leader>mm"
 }
 
+M.previous_input = {}
+
 M.code = function ()
     local prompt = "Insert Command: "
     local bufmap = vim.api.nvim_buf_set_keymap
-    vim.ui.input({ prompt = prompt }, function(input)
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    vim.ui.input({ prompt = prompt, default = M.previous_input[bufnr] }, function(input)
         if input then
             bufmap(0, "n", M.setup.input_map,
                 ":w<CR>:!" .. input .. "<CR>", { noremap = true })
+            M.previous_input[bufnr] = input
         end
     end)
 end
